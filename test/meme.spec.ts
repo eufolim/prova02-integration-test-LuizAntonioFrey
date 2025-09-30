5a718a3f3977cb7ee412137b9cbb6bad736363d9
eufolim <luiz.afc@outook.com>
2025-09-30 20:38:34 -0300

    testando cloudflare

--- test/meme.spec.ts
+++ test/meme.spec.ts
@@ -5,8 +5,9 @@ import { SimpleReporter } from '../simple-reporter';
 describe('Meme Management', () => {
   const p = pactum;
   const rep = SimpleReporter;
-  const baseUrl = 'https://api.imgflip.com/get_memes';
-  let memes = '';
+  const baseUrl = 'https://api.imgflip.com/';
+
+  let response = '';
 
   p.request.setDefaultTimeout(30000);
 
@@ -15,12 +16,42 @@ describe('Meme Management', () => {
 
   describe('Meme', () => {
     it('Get memes', async () => {
-      memes = await p
+      await p
         .spec()
-        .get(`${baseUrl}`)
+        .get(`${baseUrl}get_memes`)
         .expectStatus(StatusCodes.OK)
         .returns('body');
-      console.log(memes);
+    });
+    it('Caption image with text', async () => {
+      response = await p
+        .spec()
+        .post(`${baseUrl}caption_image`)
+        .withHeaders('Content-Type','application/x-www-form-urlencoded')
+        .withQueryParams({
+          'template_id':97984,
+          'username':'LuizAFrey',
+          'password':'TheMemes',
+          'text0':'the wizard',
+          'text1':'after fireball',
+        })
+        .expectBodyContains(`true`)
+        .returns('res.body');
+        console.log(response)
+    });
+    it('Caption image with boxes', async () =>{
+      response = await p
+      .spec()
+      .post(`${baseUrl}caption_image`)
+      .withHeaders('Content-Type','application/x-www-form-urlencoded')
+      .withQueryParams({
+        'template_id':87743020,
+        'username':'LuizAFrey',
+        'password':'TheMemes',
+        })
+      .withQueryParams('boxes[0][text]=One does not simply&boxes[1][text]=Make custom memes on the web via imgflip API&boxes[2][text]=And get the number of boxes wrong')
+      .expectBodyContains(`true`)
+      .returns('res.body')
+      console.log(response);   
     });
   });
 });
